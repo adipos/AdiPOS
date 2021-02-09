8c83c72c9179bffe1e581ddd47cb36bdc3baacce
Andres Felipe Guerrero Restrepo <andres.guerrero@qcode.co>
2021-02-08 22:22:52 -0500

    first commit

---
+++ AdiPOS/AdiPOS/App.axaml.cs
@@ -0,0 +1,29 @@
+using AdiPOS.ViewModels;
+using AdiPOS.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace AdiPOS
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
